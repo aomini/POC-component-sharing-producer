b31f8502cf5ce7138c1344f2a8c6e10fd8041db7
aomini daiki <rakesh.shrestha@hazesoft.co>
2022-03-10 17:14:43 +0545

    :rocket: develop init

--- components/hazesoft.js
+++ components/hazesoft.js
@@ -1,5 +1,5 @@
 function pocMain() {
   const h1 = document.createElement("h1");
-  h1.textContent = "Hazesoft POC v1";
+  h1.textContent = "Hazesoft POC v1-develop";
   document.body.appendChild(h1);
 }
